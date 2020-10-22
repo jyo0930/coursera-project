fe5ea5bd65ab327321feb82f6bdacfa553a9b273
jyo0930 <65009337+jyo0930@users.noreply.github.com>
2020-10-22 11:43:01 +0530

    Add files via upload

---
+++ js/javas.js
@@ -0,0 +1,613 @@
+/*html easy*/
+
+function answervisibleh1() {
+	var x = document.getElementById("hactual1");
+	var y = document.getElementById("hact1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisibleh2() {
+	var x = document.getElementById("hactual2");
+	var y = document.getElementById("hact2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisibleh3() {
+	var x = document.getElementById("hactual3");
+	var y = document.getElementById("hact3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisibleh4() {
+	var x = document.getElementById("hactual4");
+	var y = document.getElementById("hact4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisibleh5() {
+	var x = document.getElementById("hactual5");
+	var y = document.getElementById("hact5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+
+
+
+/*html medium*/
+
+function answervisibleh6() {
+	var x = document.getElementById("hactual6");
+	var y = document.getElementById("hact6");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisibleh7() {
+	var x = document.getElementById("hactual7");
+	var y = document.getElementById("hact7");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisibleh8() {
+	var x = document.getElementById("hactual8");
+	var y = document.getElementById("hact8");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisibleh9() {
+	var x = document.getElementById("hactual9");
+	var y = document.getElementById("hact9");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisibleh10() {
+	var x = document.getElementById("hactual10");
+	var y = document.getElementById("hact10");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+/*html hard*/
+
+function answervisibleh11() {
+	var x = document.getElementById("hactual11");
+	var y = document.getElementById("hact11");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisibleh12() {
+	var x = document.getElementById("hactual12");
+	var y = document.getElementById("hact12");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisibleh13() {
+	var x = document.getElementById("hactual13");
+	var y = document.getElementById("hact13");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisibleh14() {
+	var x = document.getElementById("hactual14");
+	var y = document.getElementById("hact14");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisibleh15() {
+	var x = document.getElementById("hactual15");
+	var y = document.getElementById("hact15");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+
+/*css easy*/
+
+function answervisiblec1() {
+	var x = document.getElementById("cactual1");
+	var y = document.getElementById("cact1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblec2() {
+	var x = document.getElementById("cactual2");
+	var y = document.getElementById("cact2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblec3() {
+	var x = document.getElementById("cactual3");
+	var y = document.getElementById("cact3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblec4() {
+	var x = document.getElementById("cactual4");
+	var y = document.getElementById("cact4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblec5() {
+	var x = document.getElementById("cactual5");
+	var y = document.getElementById("cact5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+
+
+
+/*css medium*/
+
+function answervisiblec6() {
+	var x = document.getElementById("cactual6");
+	var y = document.getElementById("cact6");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblec7() {
+	var x = document.getElementById("cactual7");
+	var y = document.getElementById("cact7");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblec8() {
+	var x = document.getElementById("cactual8");
+	var y = document.getElementById("cact8");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblec9() {
+	var x = document.getElementById("cactual9");
+	var y = document.getElementById("cact9");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblec10() {
+	var x = document.getElementById("cactual10");
+	var y = document.getElementById("cact10");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+/*css hard*/
+
+function answervisiblec11() {
+	var x = document.getElementById("cactual11");
+	var y = document.getElementById("cact11");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblec12() {
+	var x = document.getElementById("cactual12");
+	var y = document.getElementById("cact12");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblec13() {
+	var x = document.getElementById("cactual13");
+	var y = document.getElementById("cact13");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblec14() {
+	var x = document.getElementById("cactual14");
+	var y = document.getElementById("cact14");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblec15() {
+	var x = document.getElementById("cactual15");
+	var y = document.getElementById("cact15");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+
+/*js easy*/
+
+function answervisiblej1() {
+	var x = document.getElementById("jactual1");
+	var y = document.getElementById("jact1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblej2() {
+	var x = document.getElementById("jactual2");
+	var y = document.getElementById("jact2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblej3() {
+	var x = document.getElementById("jactual3");
+	var y = document.getElementById("jact3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblej4() {
+	var x = document.getElementById("jactual4");
+	var y = document.getElementById("jact4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblej5() {
+	var x = document.getElementById("jactual5");
+	var y = document.getElementById("jact5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+
+
+
+/*js medium*/
+
+function answervisiblej6() {
+	var x = document.getElementById("jactual6");
+	var y = document.getElementById("jact6");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblej7() {
+	var x = document.getElementById("jactual7");
+	var y = document.getElementById("jact7");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblej8() {
+	var x = document.getElementById("jactual8");
+	var y = document.getElementById("jact8");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblej9() {
+	var x = document.getElementById("jactual9");
+	var y = document.getElementById("jact9");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblej10() {
+	var x = document.getElementById("jactual10");
+	var y = document.getElementById("jact10");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+/*js hard*/
+
+function answervisiblej11() {
+	var x = document.getElementById("jactual11");
+	var y = document.getElementById("jact11");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblej12() {
+	var x = document.getElementById("jactual12");
+	var y = document.getElementById("jact12");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblej13() {
+	var x = document.getElementById("jactual13");
+	var y = document.getElementById("jact13");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblej14() {
+	var x = document.getElementById("jactual14");
+	var y = document.getElementById("jact14");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
+
+function answervisiblej15() {
+	var x = document.getElementById("jactual15");
+	var y = document.getElementById("jact15");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.setAttribute('checked', '');
+    
+  } else {
+    x.style.display = "none";
+    y.removeAttribute('checked', '');
+  }
+}
